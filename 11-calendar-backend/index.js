b036d5bf16b926e293f522ce30ce5806139ebfab
Gianluca D'Onofrio <giandonofrio@MBP-de-Gianluca.fibertel.com.ar>
2022-10-01 22:57:17 -0300

    Editing Vercel and index

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -13,10 +13,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 app.use(express.static('public'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
