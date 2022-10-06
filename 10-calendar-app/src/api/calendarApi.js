9909a35b0809addd766df15fccb3f556a41bf7d1
Gianluca D'Onofrio <giandonofrio@MacBook-Pro-de-Gianluca.local>
2022-10-06 17:22:30 -0300

    Editing env variables

--- 10-calendar-app/src/api/calendarApi.js
+++ 10-calendar-app/src/api/calendarApi.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import { getEnvVariables } from "../helpers/getEnvVariables";
 
-const { REACT_APP_API_URL } = getEnvVariables();
 
 const calendarApi = axios.create({
-  baseURL: REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 calendarApi.interceptors.request.use((config) => {
